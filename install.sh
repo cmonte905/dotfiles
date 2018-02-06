cb0468ec331745b596f7181ea62ae8c1d4249437
cmonte905 <cmonte905>
2018-02-06 03:16:03 -0800

    Adding install script

---
+++ install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# vim gets installed first? At least Vunlde repo will first
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+# With Vundle installed, can go ahead and move dotfiles moved
