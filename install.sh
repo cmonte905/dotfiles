25474a665a4aa457a550a78e3a18e54055b85e78
cemo <cemo@littledeby.cemo>
2018-02-21 01:31:59 -0800

    Changed dotfiles to make them visibe

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # vim gets installed first? At least Vunlde repo will first
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
 # With Vundle installed, can go ahead and move dotfiles moved
 # Copying xninttc file and Xresources for urxvt 
 cp .Xresources ~/.Xresources
-cp .xinitrc ~/.xinitrc
